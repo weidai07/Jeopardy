329920acf5fd44adb112f98d644901124a03495a
Andrew Bates <ABatesAccs@gmail.com>
2020-02-04 11:11:07 -0800

    add ApiHelper, Board, Category, and Question models, as well as a basic JeopardyTableController

---
+++ Jeopardy/Models/Category.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Jeopardy.Models
+{
+  public class Category
+  {
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public List<Question> Questions { get; set; }
+
+    public Category()
+    {
+        this.Questions = new List<Question>();
+    }
+
+    public static List<Question> GetQuestions(string apiSearch)
+    {
+      var apiCallTask = ApiHelper.ApiCall(apiSearch);
+      var result = apiCallTask.Result;
+
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+      this.Questions = JsonConvert.DeserializeObject<List<Question>>(jsonResponse["results"].ToString());
+    }
+  }
+}
