0b52eb5910d788d17d30c9db10da59dcff028bf2
Jake Ash <jacob.ash1998@gmail.com>
2020-02-05 15:49:07 -0800

    Try to not display links after they are clicked

--- Jeopardy/Controllers/HomeController.cs
+++ Jeopardy/Controllers/HomeController.cs
@@ -12,6 +12,37 @@ namespace Jeopardy.Controllers
     {
         public IActionResult Index()
         {
+            // bool a2 = false;
+            // bool a4 = false;
+            // bool a6 = false;
+            // bool a8 = false;
+            // bool a10 = false;
+            // bool b2= false;
+            // bool b4 = false;
+            // bool b6 = false;
+            // bool b8 = false;
+            // bool b10 = false;
+            // bool c2 = false;
+            // bool c4 = false;
+            // bool c6 = false;
+            // bool c8 = false;
+            // bool c10 = false;
+            // bool d2 = false;
+            // bool d4 = false;
+            // bool d6 = false;
+            // bool d8 = false;
+            // bool d10 = false;
+            // bool e2 = false;
+            // bool e4 = false;
+            // bool e6 = false;
+            // bool e8 = false;
+            // bool e10 = false;
+            // bool f2 = false;
+            // bool f4 = false;
+            // bool f6 = false;
+            // bool f8 = false;
+            // bool f10 = false;
+            
             return View();
         }
 
--- Jeopardy/wwwroot/js/site.js
+++ Jeopardy/wwwroot/js/site.js
@@ -2,3 +2,44 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+// let a2 = false;
+// let a4 = false;
+// let a6 = false;
+// let a8 = false;
+// let a10 = false;
+// let b2= false;
+// let b4 = false;
+// let b6 = false;
+// let b8 = false;
+// let b10 = false;
+// let c2 = false;
+// let c4 = false;
+// let c6 = false;
+// let c8 = false;
+// let c10 = false;
+// let d2 = false;
+// let d4 = false;
+// let d6 = false;
+// let d8 = false;
+// let d10 = false;
+// let e2 = false;
+// let e4 = false;
+// let e6 = false;
+// let e8 = false;
+// let e10 = false;
+// let f2 = false;
+// let f4 = false;
+// let f6 = false;
+// let f8 = false;
+// let f10 = false;
+// $(document).ready(function() {
+//     console.log("Hello there")
+//     console.log(a2);
+//     $(".cella2").click(function(e){
+//         a2 = true;
+//         console.log(a2);
+//         if (a2 === true){
+//             $(e.target).hide();
+//         }
+//     })
+// })
